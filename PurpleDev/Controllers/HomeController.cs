a104b30836a5d5e966db3e3a59923dc7e23300be
johanna <johanna@DNNVG5J>
2017-03-10 17:11:15 +0000

    AdventureWorks models

--- PurpleDev/Controllers/HomeController.cs
+++ PurpleDev/Controllers/HomeController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PurpleDev.DAL;
+using PurpleDev.Models;
 
 namespace PurpleDev.Controllers
 {
     public class HomeController : Controller
     {
+        private PurpleContext db = new PurpleContext();
         public ActionResult Index()
         {
             return View();
